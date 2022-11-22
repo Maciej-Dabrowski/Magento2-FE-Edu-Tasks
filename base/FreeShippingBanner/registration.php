8b17228812d546a3cad9fd83233b558abb761e6d
Maciej Dabrowski <maciej.dabrowski@scandiweb.com>
2022-11-22 17:06:06 +0100

    Magento2 FE Edu Tasks complete

---
+++ base/FreeShippingBanner/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Scandiweb_FreeShippingBanner',
+    __DIR__
+);
---
+++ base/Magento_Checkout/web/js/sidebar-mixin.js
@@ -0,0 +1,30 @@
+define([
+    'jquery',
+    'underscore',
+    'jquery-ui-modules/widget',
+    'mage/decorate',
+], function ($, _) {
+    'use strict';
+    return function (widget) {
+        $.widget('mage.sidebar', widget, {
+            _initContent: function () {
+                var self = this,
+                    events = {};
+
+                this.element.decorate('list', this.options.isRecursive);
+
+                /**
+                 * @param {jQuery.Event} event
+                 */
+                events['click ' + this.options.button.remove] = function (event) {
+                    self._removeItem($(event.currentTarget));
+                };
+
+                this._on(this.element, events);
+                this._calcHeight();
+            },
+        });
+        
+        return $.mage.sidebar;
+    }
+});
