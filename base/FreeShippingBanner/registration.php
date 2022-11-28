6225daedd3d3e425c05144f2438a863db14de4ba
Maciej Dabrowski <maciej.dabrowski@scandiweb.com>
2022-11-28 10:34:59 +0100

    Reworked shipping banner, not as a module. Restored missing functions from _initContent

--- base/FreeShippingBanner/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Scandiweb_FreeShippingBanner',
-    __DIR__
-);
